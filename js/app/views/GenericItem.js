4b2c22cfdac1349173ad52af28db6b90a5b623ef
web-intelligence <info@webintelligence.ie>
2014-03-05 11:37:56 +0000

    in app browser changes

--- js/app/views/GenericItem.js
+++ js/app/views/GenericItem.js
@@ -34,7 +34,6 @@ define(function (require) {
             
             }
             else{
-                alert('openming inAppBrowser');
                 window.open(href, '_blank');
             }
             
